178a70b011d422f220ff2063f771115cbbbf6dc5
Daniel Hjelseth Høyer <github@dahoiv.net>
2022-09-15 10:33:32 +0200

    Refactor (#11)
    * refactor
    
    Signed-off-by: Daniel Hjelseth Høyer <github@dahoiv.net>
    
    * refactor
    
    Signed-off-by: Daniel Hjelseth Høyer <github@dahoiv.net>
    
    Signed-off-by: Daniel Hjelseth Høyer <github@dahoiv.net>

--- custom_components/tibber_data/__init__.py
+++ custom_components/tibber_data/__init__.py
@@ -15,7 +15,7 @@ def setup(hass, config):
     """Setup component."""
 
     def ha_started(_):
-        discovery.load_platform(hass, "sensor", DOMAIN, {}, config)
+        discovery.load_platform(hass, "sensor", DOMAIN, config[DOMAIN], config)
 
     hass.bus.listen_once(EVENT_HOMEASSISTANT_START, ha_started)
 
---
+++ custom_components/tibber_data/helpers.py
@@ -0,0 +1,45 @@
+"""Helpers for the Tibber integration."""
+import logging
+import base64
+import datetime
+
+import tibber
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def get_historic_data(tibber_home: tibber.TibberHome, tibber_controller: tibber.Tibber):
+    """Get historic data."""
+    # pylint: disable=consider-using-f-string
+    query = """
+            {{
+              viewer {{
+                home(id: "{0}") {{
+                  consumption(resolution: HOURLY, last: 744, before:"{1}") {{
+                    nodes {{
+                      consumption
+                      cost
+                      from
+                      unitPrice
+                    }}
+                  }}
+                }}
+              }}
+            }}
+      """.format(
+        tibber_home.home_id,
+        base64.b64encode(datetime.datetime.now().isoformat().encode('ascii')).decode(),
+    )
+
+    if not (data := await tibber_controller.execute(query)):
+        _LOGGER.error("Could not find the data.")
+        return None
+    data = data["viewer"]["home"]["consumption"]
+    if data is None:
+        return None
+    return data["nodes"]
+
+
+# async def login(tibber_controller: tibber.Tibber, password: str):
+#     """Login to tibber."""
+#     return ""
