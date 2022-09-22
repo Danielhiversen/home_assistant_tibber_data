65e7e3e7a72755e14cd912dbc9e6264ecf0a08df
Daniel Hjelseth Høyer <github@dahoiv.net>
2022-09-22 21:42:27 +0200

    charging sensors
    Signed-off-by: Daniel Hjelseth Høyer <github@dahoiv.net>

---
+++ custom_components/tibber_data/consumption_data.py
@@ -0,0 +1,44 @@
+from homeassistant.util import dt as dt_util
+
+
+class Consumption:
+    """Consumption data."""
+
+    def __init__(self, timestamp, cons, price, cost):
+        """Initialize the data."""
+        self.timestamp = timestamp
+        self.cons = cons
+        self.price = price
+        self.cost = cost
+
+    @property
+    def day(self):
+        """Return day."""
+        return dt_util.as_local(self.timestamp).date()
+
+    def __lt__(self, other):
+        if self.cons is None and other.cons is None:
+            return self.timestamp < other.timestamp
+        if self.cons is None:
+            return True
+        if other.cons is None:
+            return False
+        return self.cons < other.cons
+
+    def __eq__(self, other):
+        return self.timestamp == other.timestamp
+
+    def __hash__(self):
+        return hash(self.timestamp)
+
+    def __radd__(self, other):
+        return other + self.cons
+
+    def __str__(self):
+        cons = f"{self.cons:.2f}" if self.cons else "-"
+        cost = f"{self.cost:.2f}" if self.cost else "-"
+        price = f"{self.price:.2f}" if self.price else "-"
+        return f"Cons({self.timestamp}, {cons}, {price}, {cost})"
+
+    def __repr__(self):
+        return self.__str__()
